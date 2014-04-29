c457cdb79119da4936f99549f9db1a445157f866
root <root@mitten.(none)>
2014-04-29 20:49:21 +0000

    huge mental jump - passing the instance of celery around

--- hello.py
+++ hello.py
@@ -1,18 +1,15 @@
 from twisted.internet import reactor, defer
 from twisted.web import server, resource
-from hello_app import app
-
-'''
-This is, Chrome browser somewhat aside, working nicely asynchronously.
-'''
-
-# run in under twisted through wsgi
 from twisted.web.wsgi import WSGIResource
 from twisted.web.server import Site
+
+# import app
+from hello_app import app
 
 resource = WSGIResource(reactor, reactor.getThreadPool(), app)
 site = Site(resource)
 
 if __name__ == '__main__':
+	print "Firing up..."
 	reactor.listenTCP( 5001, site )
-	reactor.run()
+	reactor.run()	
--- hello_app.py
+++ hello_app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, g
-import time
+
 from celery import Celery
 
+import time
+import json
+
 def make_celery(app):
-	celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+	celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'], broker=app.config['CELERY_BROKER_URL'])
 	celery.conf.update(app.config)
 	TaskBase = celery.Task
 	class ContextTask(TaskBase):
@@ -15,34 +18,53 @@ def make_celery(app):
 	return celery
 
 app = Flask(__name__)
+app.debug = True
 
 # celery updates
 app.config.update(
 	CELERY_BROKER_URL='redis://localhost:6379/0',
-	CELERY_RESULT_BACKEND='redis://localhost:6379/1'
+	CELERY_RESULT_BACKEND='redis://localhost:6379/1',
+	CELERY_RESULT_SERIALIZER='json',
 )
 
-
-
 celery = make_celery(app)
-
-
-@app.route("/")
-def index():
-	print "Starting request..."
-
-	# celery task deploying
-	count = 0
-	while count < 30:
-		result = add_together.delay(23, 42, count)
-		print result
-		count += 1	
-
-	print "Finished sleeping, returning response."
-	return "Hello World!  Celery tasks deployed foo."
-
 @celery.task()
 def add_together(a, b, count):
 	print "Starting:",count
-	time.sleep(1)
+	time.sleep(.1)
 	return a + b
+
+
+@app.route("/")
+# @app.route("/<user>/<job>")
+def index():
+	print "Starting request..."
+	# celery task deploying
+	count = 0
+	results = {}
+	while count < 100:
+		result = add_together.delay(23, 42, count)
+		print count, result
+		results[count] = str(result)
+		count += 1		
+	print "Finished sleeping, returning response."	
+	return json.dumps(results)
+
+
+@app.route("/status/<task_id>")
+def status(task_id):
+	
+	'''
+	How do we access the celery thing here?
+	'''
+	print dir(celery)
+
+	result = celery.AsyncResult(task_id)
+	state, retval = result.state, result.result
+	response_data = dict(id=task_id, status=state, result=retval)
+	
+	return json.dumps(response_data)
+
+
+	return "You are looking for {task_id}".format(task_id=task_id)
+	
